9ddf9b62a74d2d58e17ffafe957923d021385b81
Tobias Larsson <tobbe-larsson@hotmail.com>
2026-02-18 16:05:23 +0100

    Create & Debug vectorstore scripts. -WIP: Fix files in store loop.

---
+++ backend/scripts/createVectorStore.ts
@@ -0,0 +1,30 @@
+import "dotenv/config";
+
+import OpenAI from "openai";
+
+async function main() { 
+    if (process.env.OPENAI_VECTOR_STORE_ID) { 
+    console.log("Vector store already configured!"); 
+    return; 
+}
+
+if (!process.env.OPENAI_API_KEY) { 
+    throw new Error("Missing OPENAI_API_KEY"); 
+
+}
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+  const vs = await client.vectorStores.create({
+    name: "my-rag-store",
+  });
+  
+  console.log("Vector Store Created!"); 
+  console.log("ID:", vs.id);
+}
+
+
+main().catch(console.error);
+
